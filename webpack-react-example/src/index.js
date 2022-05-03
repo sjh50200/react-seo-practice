5bf8d4e95a9c3cc2e98cea416b119a33b4275758
cobe <cobe@clover.games>
2022-05-03 18:47:18 +0900

    typescript 설정 적용

--- webpack-react-example/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './component/App'
-
-const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if u use ts
-root.render(<App/>);
-// ReactDOM.render(<App/>, document.getElementById('app'))                       // dom에 render학 메인 app component, rendering 할 곳
--- webpack-react-example/webpack.config.js
+++ webpack-react-example/webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')            // index.htm
 const port = process.env.PORT || 3000;
 
 module.exports = {                                      // moduel export (옛날 방식..)
-    entry: './src/index.js',                            // 리액트 파일이 시작하는 곳
+    // entry: './src/index.js',                            // 리액트 파일이 시작하는 곳
+    entry: path.resolve(__dirname, "src/index.ts"),
     output: {                                           // bundled compiled 파일
         path: path.join(__dirname, '/dist'),            //__dirname : 현재 디렉토리, dist 폴더에 모든 컴파일된 하나의 번들파일을 넣을 예정
         filename: 'index_bundle.js'
@@ -11,12 +12,21 @@ module.exports = {                                      // moduel export (옛날
     module: {                                           // javascript 모듈을 생성할 규칙을 지정 (node_module을 제외한.js 파일을 babel-loader로 불러와 모듈을 생성
         rules: [
             {
-                test: /\.js$/,                          // .js, .jsx로 끝나는 babel이 컴파일하게 할 모든 파일
-                exclude: /node_module/,                 // node module 폴더는 babel 컴파일에서 제외
-                use:{
-                    loader: 'babel-loader'				// babel loader가 파이프를 통해 js 코드를 불러옴
-                }
-            }
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /.jsx?$/,
+                include: [path.resolve(__dirname, "src")],
+                exclude: [path.resolve(__dirname, "node_modules")],
+                loader: "babel-loader",
+            },
+            // {
+            //     test: /\.js$/,                          // .js, .jsx로 끝나는 babel이 컴파일하게 할 모든 파일
+            //     loader: 'babel-loader',				// babel loader가 파이프를 통해 js 코드를 불러옴
+            //     exclude: /node_module/,                 // node module 폴더는 babel 컴파일에서 제외
+            // },
         ]
     },
     plugins: [
@@ -29,5 +39,8 @@ module.exports = {                                      // moduel export (옛날
         port: port,
         open: true,
         hot: true,
-    }
+    },
+    resolve: {
+        extensions: [".tsx", ".json", ".js", ".jsx"],
+    },
 }
